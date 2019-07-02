d849bb78773cc52b4526349791b7fd729197c5c7
Tanya Yang <tanyang125@gmail.com>
2019-07-02 12:34:37 -0400

    potential divide by zero bug

--- kmeans-from-scratch-and-sklearn.py
+++ kmeans-from-scratch-and-sklearn.py
@@ -8,7 +8,7 @@ plt.style.use('ggplot')
 
 # Importing the dataset
 data = pd.read_csv('relative_contributions.csv')
-print('OUR DATA SHAPE', data.shape)
+print('Data shape:', data.shape)
 data.head()
 
 # Getting the values and plotting it
@@ -17,7 +17,7 @@ rightstim = data['RightStim'].values
 X = np.array(list(zip(leftstim, rightstim)))
 
 plt.scatter(leftstim, rightstim, c='black', s=7)
-plt.title("Left and Right Stimulus")
+plt.title("Relative Contributions from Left and Right Stimulus")
 plt.xlabel("Left Stimulus")
 plt.ylabel("Right Stimulus")
 
@@ -26,7 +26,7 @@ def dist(a, b, ax=1):
     return np.linalg.norm(a - b, axis=ax)
 
 # Number of clusters
-k = 4
+k = 6
 C_x = np.random.random_sample(k)
 C_y = np.random.random_sample(k)
 C = np.array(list(zip(C_x, C_y)), dtype=np.float32 )
@@ -38,11 +38,12 @@ plt.scatter(C_x, C_y, marker='*', s=200, c='g')
 
 # To store the value of centroids when it updates
 C_old = np.zeros(C.shape)
-# Cluster Lables(0, 1, 2)
+# Cluster Labels(0, 1, 2)
 clusters = np.zeros(len(X))
 # Error func. - Distance between new centroids and old centroids
 error = dist(C, C_old, None)
 # Loop will run till the error becomes zero
+cluster_count = 0
 while error != 0:
     # Assigning each value to its closest cluster
     for i in range(len(X)):
@@ -54,16 +55,19 @@ while error != 0:
     # Finding the new centroids by taking the average value
     for i in range(k):
         points = [X[j] for j in range(len(X)) if clusters[j] == i]
-        C[i] = np.mean(points, axis=0)
+        try:
+            C[i] = np.mean(points, axis=0)
+        except RuntimeWarning:
+            print('error in taking the mean')
     error = dist(C, C_old, None)
-
+    cluster_count += 1
+print("Counting how many times we reassign", cluster_count)
 colors = ['r', 'g', 'b', 'y', 'c', 'm']
 fig, ax = plt.subplots()
 for i in range(k):
         points = np.array([X[j] for j in range(len(X)) if clusters[j] == i])
         ax.scatter(points[:, 0], points[:, 1], s=7, c=colors[i])
 ax.scatter(C[:, 0], C[:, 1], marker='*', s=200, c='#050505')
-
 
 '''
 ==========================================================
